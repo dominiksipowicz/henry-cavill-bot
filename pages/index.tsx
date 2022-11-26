505ff1bca97c8905bd973ad1da7398115eb66fc5
Dom Sip <dominik.sipowicz@gmail.com>
2022-11-26 14:47:04 +0000

    make title shorter on mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,7 +38,7 @@ export default function Home() {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Henry Cavill Chat Bot
+            Henry Cavill <span className="hidden sm:inline">Chat</span> Bot
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
             The purpose of this project is to re-create a chat bot that can
