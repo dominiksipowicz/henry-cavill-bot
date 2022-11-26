66874c2c33f03bb9118bc55202ccf2e6544cf1e2
Dom Sip <dominik.sipowicz@gmail.com>
2022-11-26 23:20:43 +0000

    fix prompt new lines issue

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -13,24 +13,27 @@ const firstMessge = initialMessages[0].message
 export default async function handler(req: any, res: any) {  
 
   const prompt = req.body.prompt;
-  const defaultPrompt = `I am Henry Cavil. \n\nThis is the conversation between Henry Cavil and a news reporter.\n\nHenry Cavil: ${firstMessge}\nNews reporter: ${prompt}`;
+  const botName = 'Henry';
+  const userName = 'Gabi';
+  const defaultPrompt = `I am Henry Cavil. \n\nThis is the conversation between Henry Cavil and a news reporter.\n\n${botName}: ${firstMessge}\nNews reporter: ${prompt}\n${botName}: `;
+  const finalPrompt = process.env.OPENAI_API_KEY ? `${process.env.AI_PROMPT}${prompt}\n${botName}: ` : defaultPrompt
+  
   
   const payload = {
     model: "text-davinci-002",
-    prompt: process.env.OPENAI_API_KEY ? `${process.env.AI_PROMPT.replace(/\\\\/g, '\\')}${prompt}` : defaultPrompt,
+    prompt: finalPrompt,
     temperature: process.env.AI_TEMP ? parseFloat(process.env.AI_TEMP) : 0.7,
     max_tokens: process.env.AI_MAX_TOKENS ? parseInt(process.env.AI_MAX_TOKENS) : 1000,
     top_p: 1,
     frequency_penalty: 0,
     presence_penalty: 0,
-    stop: ["\n", "Gabi:", "Henry:"],
+    stop: [`${userName}:`, `${botName}:`, "News reporter:"],
     user: req.body?.user
   };
 
-  console.log(" === PAYLOAD = ", payload)
   const response = await openai.createCompletion(payload);
   const firstResponse = response.data.choices[0].text;
-
+  
   res.status(200).json({ text: firstResponse })
 }
 
