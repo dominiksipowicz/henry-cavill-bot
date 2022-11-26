335041511e1b0df085707578d4ba1bbbc4df013b
Dom Sip <dominik.sipowicz@gmail.com>
2022-11-26 14:50:41 +0000

    title styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,10 +37,10 @@ export default function Home() {
       </Head>
       <Container className="mt-9">
         <div className="max-w-2xl">
-          <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+          <h1 className="py-5 text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
             Henry Cavill <span className="hidden sm:inline">Chat</span> Bot
           </h1>
-          <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
+          <p className="mt-6 pb-5 text-base text-zinc-600 dark:text-zinc-400">
             The purpose of this project is to re-create a chat bot that can
             talk, joke, and hold a simple conversation based on Henry Cavill
             characters such as the Witcher, Superman, and Sherlock Holmes in the
