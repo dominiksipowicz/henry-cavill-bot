923c4317ec5bb649ff5d282646a076d5c8d08b66
Dom Sip <dominik.sipowicz@gmail.com>
2022-11-28 01:31:13 +0000

    add whole convo context to AI engine (#21)

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -1,22 +1,55 @@
 import { Configuration, OpenAIApi } from 'openai'
 
 import { initialMessages } from '../../components/Chat' 
+import { type Message } from '../../components/ChatLine'
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
-const openai = new OpenAIApi(configuration);
 
+const botName = 'Henry';
+const userName = 'News reporter';
 const firstMessge = initialMessages[0].message
 
+const openai = new OpenAIApi(configuration);
+
+
+// @TODO: unit test this. good case for unit testing
+const generatePromptFromMessages = (messages: Message[]) => {
+
+  console.log('== INITIAL messages ==', messages)
+
+  let prompt = ''
+
+  // add first user message to prompt
+  prompt += messages[1].message
+
+  // remove first conversaiton (first 2 messages)
+  const messagesWithoutFirstConvo = messages.slice(2)
+  console.log(" == messagesWithoutFirstConvo", messagesWithoutFirstConvo)
+
+  // early return if no messages
+  if (messagesWithoutFirstConvo.length == 0) {
+    return prompt
+  }
+    
+  messagesWithoutFirstConvo.forEach((message: Message) => {
+    const name = message.who === 'user' ? userName : botName
+    prompt += `\n${name}: ${message.message}`
+  })
+  return prompt
+  
+}
 
 export default async function handler(req: any, res: any) {  
 
   const prompt = req.body.prompt;
-  const botName = 'Henry';
-  const defaultPrompt = `I am Henry Cavil. \n\nThis is the conversation between Henry Cavil and a news reporter.\n\n${botName}: ${firstMessge}\nNews reporter: ${prompt}\n${botName}: `;
-  const finalPrompt = process.env.OPENAI_API_KEY ? `${process.env.AI_PROMPT}${prompt}\n${botName}: ` : defaultPrompt
+  const messages = req.body.messages;
+  const messagesPrompt = generatePromptFromMessages(messages)
+  const defaultPrompt = `I am Henry Cavil. \n\nThis is the conversation between Henry Cavil and a news reporter.\n\n${botName}: ${firstMessge}\n${userName}: ${messagesPrompt}\n${botName}: `;
+  const finalPrompt = process.env.OPENAI_API_KEY ? `${process.env.AI_PROMPT}${messagesPrompt}\n${botName}: ` : defaultPrompt
   
+  console.log(' == finalPrompt', finalPrompt)
   
   const payload = {
     model: "text-davinci-002",
@@ -26,7 +59,7 @@ export default async function handler(req: any, res: any) {
     top_p: 1,
     frequency_penalty: 0,
     presence_penalty: 0,
-    stop: [`${botName}:`, "News reporter:"],
+    stop: [`${botName}:`, `${userName}:`],
     user: req.body?.user
   };
 
